60f865969560f634638d512068eb6ca00d6341a5
NotGayo <76450243+NotGayo@users.noreply.github.com>
2024-03-20 21:09:57 +0100

    Add files via upload

---
+++ Tamagotchi - copia/Tamagotchi/src/Model/Egg.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Egg extends Tamagochi {
+	
+	public Egg() {
+		super();
+	}
+	@Override
+	public void decrementarContadorVida() {
+		//
+	}
+	@Override
+	public void decrementarContadorComida() {
+		//
+	}
+	
+}
